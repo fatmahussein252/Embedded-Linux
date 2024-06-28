54a69dc281c7621aaeb114040e9066502f5ba33a
Fatma <husseinfatma523@gmail.com>
2024-06-28 18:36:57 +0300

    deleted wrong files

--- 01python/session4/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-while pygame.mixer.get_busy():
-    pygame.time.wait(100)  # Wait 100 milliseconds
--- 01python/session4/try.py
+++
@@ -1,7 +0,0 @@
-from pydub import AudioSegment
-f="output.mp3"
-# Open the sound file
-sound = AudioSegment.from_mp3(f)
-
-# Play the sound
-sound.play()
